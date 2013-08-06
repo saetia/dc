25188e24f5bdd51f287760bb88d36561f23903a5
Joel Glovacki <saetia@gmail.com>
2013-08-06 12:47:30 -0500

    initial commit

---
+++ Conversions/CenterViewController.h
@@ -0,0 +1,14 @@
+//
+//  CenterViewController.h
+//  Conversions
+//
+//  Created by Joel Glovacki on 7/24/13.
+//  Copyright (c) 2013 Dunmore. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "UIViewController+MMDrawerController.h"
+
+@interface CenterViewController : UIViewController
+
+@end
